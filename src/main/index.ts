4eaa5b42452217935d66d7a254f773a9f3304c75
Julio <soul.unleashed13@gmail.com>
2025-08-11 09:51:49 -0600

    Correccion en la recepcion de datos

--- src/main/index.ts
+++ src/main/index.ts
@@ -87,33 +87,37 @@ app.whenReady().then(() => {
     //Receptor de datos, canal de datos
     peer.on('data', (data) => {
       console.log('data recibida', data);
+
       try {
-        if (typeof data === 'string') {
-          if(isJsonString(data)) {
-            const jsonData = JSON.parse(data);
-            console.log('data (JSON)', jsonData);
+        const _data = data.toString();
 
-            if(jsonData.type === 'file-download') {
-              const { name, size } = jsonData;
-              const savePath = path.join(app.getPath("downloads"), name);
-              expectedSize = size;
-              receivedSize = 0;
+        if(isJsonString(_data)) {
+          const jsonData = JSON.parse(_data);
+          console.log('data (JSON)', jsonData);
 
-              fileStream = fs.createWriteStream(savePath);
+          if(jsonData.type === 'file-download') {
+            const { name, size } = jsonData;
+            const savePath = path.join(app.getPath("downloads"), name);
+            expectedSize = size;
+            receivedSize = 0;
 
-            }
-          }else{
-            if(data == '__END__') {
-                fileStream?.end();
-                console.log("✅ Archivo recibido y guardado correctamente.");
-                fileStream = null;
-                expectedSize = 0;
+            fileStream = fs.createWriteStream(savePath);
 
-            }
+          }
+        }else{
+          if(data == '__END__') {
+              fileStream?.end();
+              console.log("✅ Archivo recibido y guardado correctamente.");
+              fileStream = null;
+              expectedSize = 0;
+
           }
         }
+      } catch (error) {
 
-        if (fileStream) {
+      }
+
+      if (fileStream) {
             fileStream.write(data);
             receivedSize += data.length;
 
@@ -123,9 +127,6 @@ app.whenReady().then(() => {
                 fileStream = null;
             }
         }
-      } catch (error) {
-        console.error('Error al procesar los datos:', error);
-      }
     });
 
     // Manejar errores
